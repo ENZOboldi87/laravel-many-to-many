3dae32bf877f4da1438d0ebbf1f87efb76da9cfc
Giuseppe Rotolo <rotolo37@gmail.com>
2020-09-12 16:02:28 +0200

    1.0

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.layouts')
 @section('content')
-	<h1>sono su edit</h1>
-
+	<div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="text-center">Edit</h1>
+      </div>
+    </div>
 @if ($errors->any())
 	<div class="alert alert-danger">
 		<ul>
@@ -11,34 +15,42 @@
 		</ul>
 	</div>
 @endif
+<div class="row">
+	<div class="col-lg-12">
+		<div class="">
+			<form action="{{route('cars.update', $car)}}" method="post">
+			  @csrf
+			  @method('PUT')
+			  <div class="">
+			    <label>Manifacturer:</label><br>
+			    <input type="text" name="manifacturer" value="{{ $car->manifacturer}}" placeholder="manifacturer">
+			  </div>
+			  <div class="">
+			    <label>Year:</label><br>
+			    <input type="number" name="year" value="{{ $car->year}}" placeholder="year">
+			  </div>
+			  <div class="">
+			    <label>Engine:</label><br>
+			    <input type="text" name="engine" value="{{ $car->engine}}" placeholder="engine">
+			  </div>
+			  <div class="">
+			    <label>Plate:</label><br>
+			    <input type="text" name="plate" value="{{ $car->plate}}" placeholder="plate">
+			  </div>
+			  <div class="chekboxes">
+			    <span>Type:</span>
+			    @foreach ($tags as $tag)
+			      <div>
+			        <input type="checkbox" name="tags[]" {{ ($car->tags->contains($tag)) ? 'checked' : '' }} value="{{$tag->id}}">
+			        <label>{{$tag->name}}</label>
+			      </div>
+			    @endforeach
+			  </div>
+			</form>
+		</div>
+	</div>
+</div>
+<a class="btn btn-light" href="{{ route('cars.index')}}">go back</a>
+</div>
 
-<form action="{{route('cars.update', $car)}}" method="post">
-  @csrf
-  @method('PUT')
-  <div class="">
-    <label>Manifacturer:</label><br>
-    <input type="text" name="manifacturer" value="{{ $car->manifacturer}}" placeholder="manifacturer">
-  </div>
-  <div class="">
-    <label>Year:</label><br>
-    <input type="number" name="year" value="{{ $car->year}}" placeholder="year">
-  </div>
-  <div class="">
-    <label>Engine:</label><br>
-    <input type="text" name="engine" value="{{ $car->engine}}" placeholder="engine">
-  </div>
-  <div class="">
-    <label>Plate:</label><br>
-    <input type="text" name="plate" value="{{ $car->plate}}" placeholder="plate">
-  </div>
-  <div class="chekboxes">
-    <span>Type:</span>
-    @foreach ($tags as $tag)
-      <div>
-        <input type="checkbox" name="tags[]" {{ ($car->tags->contains($tag)) ? 'checked' : '' }} value="{{$tag->id}}">
-        <label>{{$tag->name}}</label>
-      </div>
-    @endforeach
-  </div>
-</form>
 @endsection
--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -1,25 +1,40 @@
 @extends('layouts.layouts')
 @section('content')
-<h1>Dettagli Auto</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="text-center">Details</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="">
+          <h2> {{ $car->manifacturer}} {{ $car->engine }}</h2>
+          <div>
+            @foreach ($car->tags as $tag)
+              <span>Type: {{$tag->name}}</span>
+            @endforeach
+          </div>
+          <ul>
+            <li>Year: {{ $car->year }}</li>
+            <li>Plate: {{ $car->plate }}</li>
+          </ul>
 
-<h2> {{ $car->manifacturer}} {{ $car->engine }}</h2>
-<div>
-  @foreach ($car->tags as $tag)
-    <span>Type: {{$tag->name}}</span>
-  @endforeach
-</div>
-<ul>
-  <li>Year: {{ $car->year }}</li>
-  <li>Plate: {{ $car->plate }}</li>
-</ul>
+          <h3>Owner details</h3>
+          <p>
+            <b>{{ $car->user->name}}</b>
+          </p>
+          <p>
+            For contacts: <br>
+            <i>{{ $car->user->email}}</i>
+          </p>
+        </div>
+      </div>
+    </div>
+    <a class="btn btn-light" href="{{ route('cars.index')}}">go back</a>
+  </div>
 
-<h3>Owner details</h3>
-<p>
-  <b>{{ $car->user->name}}</b>
-</p>
-<p>
-  For contacts: <br>
-  <i>{{ $car->user->email}}</i>
-</p>
-<a href="{{ route('cars.index')}}">go back</a>
+
+
+
 @endsection
