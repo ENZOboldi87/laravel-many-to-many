7324eab9e4f464c76276058fb4930e9de74b10e6
Giuseppe Rotolo <rotolo37@gmail.com>
2020-09-12 16:53:40 +0200

    1.3

--- database/seeds/CarsTableSeeder.php
+++ database/seeds/CarsTableSeeder.php
@@ -13,16 +13,19 @@ class CarsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+      $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+
+
       for ($i=0; $i < 10; $i++) {
         $new_car = new Car();
-        $new_car->manifacturer = $faker->randomElement(['ford' , 'bmw', 'fiat', 'audi', 'alfa romeo', 'auto bianchi', 'ferrari']);
-        $new_car->year = $faker->year;
-        $new_car->engine = $faker->randomElement(['1200', 'v6', 'v8', 'v12']);
-        $new_car->plate = $faker->word;
+        $new_car->manifacturer = $faker->vehicle;
+        $new_car->year = $faker->biasedNumberBetween(1998,2017, 'sqrt');
+        $new_car->engine = $faker->vehicleModel;
+        $new_car->plate = $faker->vehicleRegistration();
         $new_car->user_id = rand(1,5);
         $new_car->save();
 
-        $new_car->tags()->attach(rand(1,4));
+        // $new_car->tags()->attach(rand(1,4));
       }
     }
 }
